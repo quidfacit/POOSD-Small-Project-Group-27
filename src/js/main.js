d1a48e02aeb8196b27ebcc150eeb1719cf6d2491
Tanndlin <tanndlin@gmail.com>
2022-08-28 15:26:48 -0400

    Basic layout and functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,46 @@
 console.log('Hello World!');
+
+function addEntry(firstName, lastName, age) {
+  const table = document.getElementById('contactsTable');
+
+  // Add the new row under the header row
+  const row = table.insertRow(1);
+
+  const cell1 = row.insertCell(0);
+  const cell2 = row.insertCell(1);
+  const cell3 = row.insertCell(2);
+
+  // TODO: prevent code injection
+  cell1.innerHTML = firstName;
+  cell2.innerHTML = lastName;
+  cell3.innerHTML = age;
+}
+
+function clearTable() {
+  // Remove all children except the headers
+  const tbody = document.getElementById('contactsTable').children[0];
+  tbody.replaceChildren(tbody.children[0]);
+}
+
+function search() {
+  const searchTerm = document.getElementById('searchInput').value;
+  console.log(searchTerm);
+
+  // clearTable();
+
+  // Get results from API
+  // const apiResult = apiSearch(searchTerm)
+  //
+  // for (const contact of apiResult) {
+  //   const { firstName, lastName, age } = contact;
+  //   addEntry(firstName, lastName, age);
+  // }
+}
+
+function createContact() {
+  const firstName = document.getElementById('firstNameInput');
+  const lastName = document.getElementById('lastNameInput');
+  const age = document.getElementById('ageInput');
+
+  // send to api
+}
