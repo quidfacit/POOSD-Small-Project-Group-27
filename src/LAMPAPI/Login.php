2edf70e8f98e9194a9413c191a20ce025b1ddbe9
quidfacit <77417740+quidfacit@users.noreply.github.com>
2022-09-02 13:17:59 -0400

    Create Login.php

---
+++ src/LAMPAPI/Login.php
@@ -0,0 +1,21 @@
+<?php
+    $res = false;
+    $request = json_decode(file_get_contents("php://input"), true);
+    $login = $request->Login;
+    $pass = $request->Password;
+    $conn = new mysqli("localhost", "root", "27isBest", "main");
+
+    if($conn->connect_error = null){
+        $data = $conn->query("SELECT * FROM Users WHERE Login = $login AND Password = $pass");
+
+        if($data->num_rows != 0){
+            $res = true;
+        }
+
+        $conn->close();
+    }
+
+    header("Content-Type: application/json; charset = utf-8");
+
+    print($res);
+?>
