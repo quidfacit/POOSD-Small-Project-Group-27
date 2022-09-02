f30009af4927534f0f86b13a3e78d3b7b28a1f76
quidfacit <77417740+quidfacit@users.noreply.github.com>
2022-09-02 15:53:52 -0400

    Create AddContact.php

---
+++ src/LAMPAPI/AddContact.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<title>Group 27 Contact Manager</title>
+	<script type="text/javascript" src="js/main.js"></script>
+    <link href="css/styles.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+</head>
+<body>
+
+<h1 id="title">COP 4331</h1>
+
+<div id="loginDiv">
+	<span id="inner-title">PLEASE LOG IN</span><br />
+	<input type="text" id="loginName" placeholder="Username" /><br />
+	<input type="password" id="loginPassword" placeholder="Password"/><br />
+	<button type="button" id="loginButton" class="buttons" onclick="doLogin();"> Do It </button>
+	<span id="loginResult"></span>
+</div>
+
+</body>
+</html>
