53eec09712ec8afe41d093bf329ee40fee1fe346
alann-nguyen <70539811+alann-nguyen@users.noreply.github.com>
2022-09-13 12:28:42 -0400

    Update login.js

--- src/js/login.js
+++ src/js/login.js
@@ -1,42 +1,42 @@
 function doRegister() {
-    // send to api
-      const urlBase = 'http://contacts27.com/LAMPAPI';
-      const extension = 'php';
+                          // send to api
+                            const urlBase = 'http://contacts27.com/LAMPAPI';
+                            const extension = 'php';
 
-      const payload = JSON.stringify({
-          Login: document.getElementById("regUsername").value,
-          Password: document.getElementById("regPassword").value,
-          FirstName: document.getElementById("firstName").value,
-          LastName: document.getElementById("lastName").value  
-      });
+                            const payload = JSON.stringify({
+                                Login: document.getElementById("regUsername").value,
+                                Password: document.getElementById("regPassword").value,
+                                FirstName: document.getElementById("firstName").value,
+                                LastName: document.getElementById("lastName").value  
+                            });
 
-      let url = urlBase + "/Register." + extension;
+                            let url = urlBase + "/Register." + extension;
 
-      let xhr = new XMLHttpRequest();
-      xhr.open("POST", url, true);
-      xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-      try {
-          xhr.onreadystatechange = function () {
-              if (this.readyState == 4 && this.status == 200) {
-                  const { Error: err } = JSON.parse(xhr.responseText);
-                  if (err) {
-                      throw new Error(err);
-                  }
+                            let xhr = new XMLHttpRequest();
+                            xhr.open("POST", url, true);
+                            xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+                            try {
+                                xhr.onreadystatechange = function () {
+                                    if (this.readyState == 4 && this.status == 200) {
+                                        const { Error: err } = JSON.parse(xhr.responseText);
+                                        if (err) {
+                                            throw new Error(err);
+                                        }
 
-                  console.log("Successfully registered user.");
+                                        console.log("Successfully registered user.");
 
-                  //Check if the user exists already
-              }
+                                        //Check if the user exists already
+                                    }
 
-          // Clear fields
-              document.getElementById("firstName").value = "";
-              document.getElementById("lastName").value = "";
-              document.getElementById("regUsername").value = "";
-              document.getElementById("regPassword").value = "";
-          };
+                                // Clear fields
+                                    document.getElementById("firstName").value = "";
+                                    document.getElementById("lastName").value = "";
+                                    document.getElementById("regUsername").value = "";
+                                    document.getElementById("regPassword").value = "";
+                                };
 
-          xhr.send(payload);
-      } catch (e) {
-          console.error(e);
-      }     
-  }
+                                xhr.send(payload);
+                            } catch (e) {
+                                console.error(e);
+                            }     
+                        }
